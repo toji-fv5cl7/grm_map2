bc57e402bbb5aaa99665990319642f6d1bac4a5e
tojio <firebirdv.toji.fv5cl7@gmail.com>
2020-03-20 17:11:45 +0900

    これで画像が表示されるハズだったのに

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,7 @@
   class MapsController < ApplicationController
+  def index
+    @maps = Map.all
+  end
 
   def new
     @map = Map.new # フォーム用の空のインスタンスを生成する。
@@ -7,8 +10,12 @@
   def create
     @map = Map.new(map_params) # ストロングパラメータを引数に
     @map.apsave # saveをしてデータベースに保存する。
-    redirect_to @map # showページにリダイレクト
+    redirect_to @map, notice: '投稿を保存しました' # showページにリダイレクト
   end
+  def show
+    @maps = Map.find(params[:id])
+  end
+
 
   private
 
